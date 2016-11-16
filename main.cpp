f41fa5b12e1cbbfba58ed2ffc8ed567ac1321fc4
featherW <diefrom.me@gmail.com>
2016-11-16 04:29:37 -0800

    Add qss

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile qss(":/res/default.qss");
+    qss.open(QFile::ReadOnly);
+    qApp->setStyleSheet(qss.readAll());
+    qss.close();
+
     MainWindow w;
     w.initWindow();
     w.show();
